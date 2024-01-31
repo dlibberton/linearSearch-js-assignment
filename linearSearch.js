74b0bf16154805417c5458b0e48fefe39ec6de98
dlibberton <drew.libberton@gmail.com>
2024-01-31 17:43:20 -0500

    add linear search function

--- linearSearch.js
+++ linearSearch.js
@@ -1,4 +1,9 @@
-function linearSearch(searchTerm, arr) {changes
+function linearSearch(searchTerm, arr) {
+  for (let i = 0; i < arr.length; i++){
+    if (arr[i] === searchTerm){
+      return i;
+    }
+  }
   return undefined;
 }
 
